32ef1260e0b543c94cb940fa099eaac047f32343
mlyundin <mlyundin@gmail.com>
2023-02-28 21:51:57 +0300

    Rename service to domain

---
+++ loms/internal/domain/create_order.go
@@ -0,0 +1,11 @@
+package domain
+
+import (
+	"context"
+
+	"route256/loms/pkg/model"
+)
+
+func (s *domainmodel) CreateOrder(ctx context.Context, user int64, items []*model.Item) (int64, error) {
+	return 1, nil
+}
---
+++ loms/internal/domain/order_payed.go
@@ -0,0 +1,9 @@
+package domain
+
+import (
+	"context"
+)
+
+func (s *domainmodel) OrderPayed(ctx context.Context, orderId int64) error {
+	return nil
+}
