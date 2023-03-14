6ba0abb7cdd18a12915fc352373fb25449b3dda4
mlyundin <mlyundin@gmail.com>
2023-03-14 09:59:52 +0300

    Add rate limiter to products

---
+++ libs/interceptors/ratelimiter.go
@@ -0,0 +1,40 @@
+package interceptors
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type RateLimiter interface {
+	WaitN(ctx context.Context, n int) (err error)
+}
+
+type clientRateLimiterInterceptor struct {
+	RateLimiter
+}
+
+func NewClientRateLimiterInterceptor(rateLimiter RateLimiter) *clientRateLimiterInterceptor {
+	return &clientRateLimiterInterceptor{rateLimiter}
+}
+
+func (i *clientRateLimiterInterceptor) Intercept(
+	ctx context.Context,
+	method string,
+	req interface{},
+	reply interface{},
+	cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker,
+	opts ...grpc.CallOption,
+) error {
+	start := time.Now()
+	i.WaitN(ctx, 1)
+	end := time.Now()
+
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	fmt.Printf("Invoked RPC method=%s; Wait=%s; Error=%v\n", method, end.Sub(start), err)
+
+	return err
+}
